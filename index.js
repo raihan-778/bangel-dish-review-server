c96155e598381ba3352b219e2f197915f523c316
raihan-778 <raihanarif778@gmail.com>
2022-11-09 09:34:14 +0600

    get api created to get data from mongodb

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+//mongodb conntection setup
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRET_KEY}@cluster0.jz1qjld.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+    const dishCollection = client.db("bengleDishDb").collection("services");
+    const reviewCollection = client.db("bengleDishDb").collection("reviews");
+
+    app.get("/dishes", async (req, res) => {
+      const query = {};
+      const cursor = dishCollection.find(query);
+      const dishes = await cursor.toArray();
+      res.send(dishes);
+    });
+  } finally {
+  }
+};
+run().catch((err) => console.error(err));
 
 //middleware
 app.use(cors());
